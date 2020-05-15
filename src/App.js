ab765a6b8dc5288cc84746ddff3d6dfc2d7881bb
stanley dsouza <lancylotdsouza@gmail.com>
2020-05-15 20:18:56 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -1,40 +1,45 @@
 import React from 'react';
 
-import { Cards, Chart, CountryPicker} from './components';
+import { Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 import coronaImg from './images/image.png';
 import ReactGA from 'react-ga';
 
-class App extends React.Component{
-    state={
-        data:{},
-        country:'',
+class App extends React.Component {
+    state = {
+        data: {},
+        country: '',
     }
-     initializeReactGA = ()=> {
+    initializeReactGA = () => {
         // ReactGA.initialize('UA-162979718-1');
         // ReactGA.pageview('/');
     }
-    async componentDidMount(){
+    async componentDidMount() {
         const fetchedData = await fetchData();
-        this.setState({data:fetchedData});
+        this.setState({ data: fetchedData });
     }
-    handleCountryChange = async (country)=>{
+    handleCountryChange = async (country) => {
         const fetchedData = await fetchData(country);
         console.log(fetchedData);
-        this.setState({data:fetchedData,country:country});
+        this.setState({ data: fetchedData, country: country });
     }
-    render(){
+    render() {
         ReactGA.initialize('UA-162979718-1');
         ReactGA.pageview('/');
-        const { data,country } = this.state;
-        return(
+        const { data, country } = this.state;
+        return (
             <div className={styles.container}>
-                <img className={styles.image} src={coronaImg} alt="COVID-19"/>
-              <Cards data={data} />
-              <CountryPicker handleCountryChange={this.handleCountryChange}/>
-              <Chart data={data} country={country}/>
+                <img className={styles.image} src={coronaImg} alt="COVID-19" />
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
+                <div class={styles.footer}>
+                <span>
+                    Created By  <a href="https://dsouzastan30.gitlab.io/stanleyportfolio/"> Stanley Dsouza</a>
+                </span>
             </div>
+            </div >
         );
     }
 }
